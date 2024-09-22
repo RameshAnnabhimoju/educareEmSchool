457ad572e2e034a5b34760a34ee394736a2ad4ff
RameshAnnabhimoju <ramesh.annabhimoju@gmail.com>
2024-09-22 16:46:01 +0530

    bug fix

--- src/pages/ConcludedEvents/CE-MSPGallery/CE-MSPGallery.tsx
+++ src/pages/ConcludedEvents/CE-MSPGallery/CE-MSPGallery.tsx
@@ -1,60 +1,65 @@
-import { SetStateAction, JSXElementConstructor, Key, ReactElement, ReactNode, ReactPortal, useState } from 'react'
-import './CE-MSPGallery.css'
-
-const MSPGallery = ({images}) => {
+import { SetStateAction, useState } from "react";
+import "./CE-MSPGallery.css";
+type imagesType = { img: string }[];
+const MSPGallery = ({ images }: { images: imagesType }) => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [openModal, setOpenModal] = useState(false);
 
-  const handleOpenModal = (index: SetStateAction<number>) =>{
-    setSlideNumber(index)
-    setOpenModal(true)
-  }
+  const handleOpenModal = (index: SetStateAction<number>) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+  };
 
-  const handleCloseModal = () =>{
+  const handleCloseModal = () => {
     setOpenModal(false);
-  }
+  };
 
   const prevSlide = () => {
-    slideNumber === 0 ?
-     setSlideNumber( images.length - 1) :
-     setSlideNumber (slideNumber - 1)
-  }
+    slideNumber === 0
+      ? setSlideNumber(images.length - 1)
+      : setSlideNumber(slideNumber - 1);
+  };
   const nextSlide = () => {
-    slideNumber + 1 === images.length ?
-     setSlideNumber(1) :
-     setSlideNumber(slideNumber + 1)
-  }
+    slideNumber + 1 === images.length
+      ? setSlideNumber(1)
+      : setSlideNumber(slideNumber + 1);
+  };
 
   return (
     <div>
-      {openModal &&
-          <div className="sliderWrap">
-            <a className="btnClose" onClick={handleCloseModal} >&times;</a>
-            <a className="btnPrev" onClick={prevSlide} >&#10094;</a>
-            <a className="btnNext" onClick={nextSlide}>&#10095;</a>
-            <div className='fullScreenImage'>
-              <img src={images[slideNumber].img} alt="" />
-            </div>
+      {openModal && (
+        <div className="sliderWrap">
+          <a className="btnClose" onClick={handleCloseModal}>
+            &times;
+          </a>
+          <a className="btnPrev" onClick={prevSlide}>
+            &#10094;
+          </a>
+          <a className="btnNext" onClick={nextSlide}>
+            &#10095;
+          </a>
+          <div className="fullScreenImage">
+            <img src={images[slideNumber].img} alt="" />
           </div>
-      }
-      <div className='concluded-events-galleryWarp'>
-        {
-          images && images.map((slide: { img: string | undefined; text: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }, index: Key | null | undefined) =>{
-            return(
+        </div>
+      )}
+      <div className="concluded-events-galleryWarp">
+        {images &&
+          images.map((slide, index: number) => {
+            return (
               <div
-               className='concluded-events-galleryWarp-item'
-               key={index}
-               onClick={() => handleOpenModal(index)}>
-               
-              <img src={slide.img} alt="asdasdf" />
-              <h6>{slide.text}</h6>
+                className="concluded-events-galleryWarp-item"
+                key={index}
+                onClick={() => handleOpenModal(index)}
+              >
+                <img src={slide.img} alt="asdasdf" />
+                {/* <h6>{slide.text}</h6> */}
               </div>
-            )
-          })
-        }
+            );
+          })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MSPGallery
+export default MSPGallery;
--- src/pages/InfrastructureFacility/MSPGallery/MSPGallery.tsx
+++ src/pages/InfrastructureFacility/MSPGallery/MSPGallery.tsx
@@ -1,60 +1,65 @@
-import { useState } from 'react'
-import './MSPGallery.css'
-
-const MSPGallery = ({images}) => {
+import { useState } from "react";
+import "./MSPGallery.css";
+type imagesType = { img: string }[];
+const MSPGallery = ({ images }: { images: imagesType }) => {
   const [slideNumber, setSlideNumber] = useState(0);
   const [openModal, setOpenModal] = useState(false);
 
-  const handleOpenModal = (index) =>{
-    setSlideNumber(index)
-    setOpenModal(true)
-  }
+  const handleOpenModal = (index: number) => {
+    setSlideNumber(index);
+    setOpenModal(true);
+  };
 
-  const handleCloseModal = () =>{
+  const handleCloseModal = () => {
     setOpenModal(false);
-  }
+  };
 
   const prevSlide = () => {
-    slideNumber === 0 ?
-     setSlideNumber( images.length - 1) :
-     setSlideNumber (slideNumber - 1)
-  }
+    slideNumber === 0
+      ? setSlideNumber(images.length - 1)
+      : setSlideNumber(slideNumber - 1);
+  };
   const nextSlide = () => {
-    slideNumber + 1 === images.length ?
-     setSlideNumber(1) :
-     setSlideNumber(slideNumber + 1)
-  }
+    slideNumber + 1 === images.length
+      ? setSlideNumber(1)
+      : setSlideNumber(slideNumber + 1);
+  };
 
   return (
     <div>
-      {openModal &&
-          <div className="sliderWrap">
-            <a className="btnClose" onClick={handleCloseModal} >&times;</a>
-            <a className="btnPrev" onClick={prevSlide} >&#10094;</a>
-            <a className="btnNext" onClick={nextSlide}>&#10095;</a>
-            <div className='fullScreenImage'>
-              <img src={images[slideNumber].img} alt="" />
-            </div>
+      {openModal && (
+        <div className="sliderWrap">
+          <a className="btnClose" onClick={handleCloseModal}>
+            &times;
+          </a>
+          <a className="btnPrev" onClick={prevSlide}>
+            &#10094;
+          </a>
+          <a className="btnNext" onClick={nextSlide}>
+            &#10095;
+          </a>
+          <div className="fullScreenImage">
+            <img src={images[slideNumber].img} alt="" />
           </div>
-      }
-      <div className='galleryWarp'>
-        {
-          images && images.map((slide, index) =>{
-            return(
+        </div>
+      )}
+      <div className="galleryWarp">
+        {images &&
+          images.map((slide, index) => {
+            return (
               <div
-               className='single'
-               key={index}
-               onClick={() => handleOpenModal(index)}>
-               
-              <img src={slide.img} alt="asdasdf" />
-              <h6>{slide.text}</h6>
+                className="single"
+                key={index}
+                onClick={() => handleOpenModal(index)}
+              >
+                <img src={slide.img} alt="asdasdf" />
+                {/* <h6>{slide.text}</h6> */}
               </div>
-            )
-          })
-        }
+            );
+          })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MSPGallery
+export default MSPGallery;
